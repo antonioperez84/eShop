225f4b2f551e31f97614bd1b3e3eb3546ebce3eb
Antonio Pérez Moreno <119811357+antonioperez84@users.noreply.github.com>
2023-04-21 17:52:47 +0200

    Update config.js

--- server/config.js
+++ server/config.js
@@ -3,5 +3,4 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/ProductosDB"
-//export const MONGODB_URI = 'mongodb+srv://antonio.vecwlnu.mongodb.net/ProductosDB' //'mongodb://antonioperez84:'+ encodeURIComponent('v2UmRNz5_rff3%i')+ '@antonio.vecwlnu.mongodb.net/?retryWrites=true&w=majority'
 export const PORT = process.env.PORT || 4000 
